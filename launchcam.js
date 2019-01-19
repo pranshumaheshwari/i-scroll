197ad4de5144cde022e4aa58bb9a6339cce7c5af
Divyansh Choudhary <divyanshchoudhary99@gmail.com>
2019-01-19 06:09:08 +0530

    made basic extension

---
+++ launchcam.js
@@ -0,0 +1,3 @@
+document.getElementById("scan").addEventListener("click",()=>{
+    newwindow = window.open("popup.html", "_blank", "resizable=yes, scrollbars=yes, titlebar=yes, width=800, height=900, top=10, left=10");
+});
