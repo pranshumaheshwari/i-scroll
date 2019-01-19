2bb21cc40892929960c25a8ffc782a23e2550315
Divyansh Choudhary <divyanshchoudhary99@gmail.com>
2019-01-19 09:51:42 +0530

    added few shortcuts

--- launchcam.js
+++ launchcam.js
@@ -1,3 +1,53 @@
 document.getElementById("scan").addEventListener("click",()=>{
-    newwindow = window.open("popup.html", "_blank", "resizable=yes, scrollbars=yes, titlebar=yes, width=800, height=900, top=10, left=10");
+    chrome.tabs.create({url:"popup.html",active:false},()=>{});
 });
+
+document.getElementById("go_f").addEventListener("click",TabForward);
+document.getElementById("go_b").addEventListener("click",TabBackward);
+document.getElementById("newTab").addEventListener("click",newTab);
+document.getElementById("removeTab").addEventListener("click",removeTab);
+
+function removeTab() {
+    chrome.tabs.query({active:true,currentWindow:true},(tab)=>{
+        chrome.tabs.remove([tab[0].id]);
+    })
+}
+
+function newTab() {
+    chrome.tabs.create({active:true});
+}
+
+function TabForward() {
+    chrome.tabs.query({currentWindow:true},(tab)=>{
+        console.log(tab);
+        chrome.tabs.query({currentWindow:true,active:true},(tab2)=>{
+            console.log("current tab = ");
+            console.log(tab2);
+            let pos =0;
+            while(pos<tab.length && tab[pos].id!=tab2[0].id){
+                pos++;
+            }
+            pos++;
+            pos = pos%tab.length;
+            chrome.tabs.highlight({tabs:[pos]});
+        });
+    });
+}
+
+function TabBackward() {
+    chrome.tabs.query({currentWindow:true},(tab)=>{
+        console.log(tab);
+        chrome.tabs.query({currentWindow:true,active:true},(tab2)=>{
+            console.log("current tab = ");
+            console.log(tab2);
+            let pos =0;
+            while(pos<tab.length && tab[pos].id!=tab2[0].id){
+                pos++;
+            }
+            pos--;
+            pos = pos+tab.length;
+            pos = pos%tab.length;
+            chrome.tabs.highlight({tabs:[pos]});
+        });
+    });
+}
